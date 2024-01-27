c5767410555fc7a00f937ed95973cf0015e70b66
amir mohammad shirin <45653153+amirMohammadShirin@users.noreply.github.com>
2024-01-27 23:00:14 +0330

    create happy scenario

--- app/src/main/java/com/example/navdemo/MainActivity.kt
+++ app/src/main/java/com/example/navdemo/MainActivity.kt
@@ -16,37 +16,11 @@ import androidx.navigation.fragment.findNavController
 
 class MainActivity() : AppCompatActivity() {
 
-    lateinit var txtStart: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        txtStart = findViewById<TextView>(R.id.txtStart)
-        supportFragmentManager.commit {
-            setReorderingAllowed(true)
-            addToBackStack(Fragment1::class.java.simpleName)
-            add(R.id.nav_host_fragment, Fragment1(), "Fragment1")
-        }
-        txtStart.setOnClickListener {
-            navigate(R.id.action_fragment1_to_fragment2)
-        }
     }
 
-
-    fun navigate(action: Int) {
-        Navigation.findNavController(findViewById(R.id.nav_host_fragment)).navigate(action)
-    }
-
-    fun getHeight(): Int {
-        val displayMetrics = DisplayMetrics()
-        windowManager?.defaultDisplay?.getMetrics(
-            displayMetrics
-        )
-        return displayMetrics.heightPixels ?: 1
-    }
-
-    override fun onBackPressed() {
-        findNavController(R.id.nav_host_fragment).popBackStack()
-    }
 
 }
--- app/src/main/java/com/example/navdemo/test/TestFragment1.kt
+++ app/src/main/java/com/example/navdemo/test/TestFragment1.kt
@@ -1,14 +1,28 @@
 package com.example.navdemo.test
 
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.NOT_FOCUSABLE
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.animation.doOnEnd
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.navdemo.CustomFragmentState
 import com.example.navdemo.R
 import com.example.navdemo.navigate
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
+import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_EXPANDED
+import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_HIDDEN
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 /**
  * Created by aShirin on 1/27/2024.
@@ -16,6 +30,12 @@ import com.example.navdemo.navigate
 class TestFragment1 : Fragment() {
 
     private val TAG = "TestFragment1"
+
+    private lateinit var firstBottomSheet: LinearLayout
+    private lateinit var secondBottomSheet: LinearLayout
+
+    private var firstBehavior: BottomSheetBehavior<LinearLayout>? = null
+    private var secondBehavior: BottomSheetBehavior<LinearLayout>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,62 +45,86 @@ class TestFragment1 : Fragment() {
         println("$TAG onCreateView")
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        println("$TAG onCreate")
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        println("$TAG onAttach")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        println("$TAG onDestroy")
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        println("$TAG onDestroyView")
-    }
-
-    override fun onStart() {
-        super.onStart()
-        println("$TAG onStart")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        println("$TAG onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        println("$TAG onStop")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        println("$TAG onResume")
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        println("$TAG onDetach")
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<TextView>(R.id.txtTest1).setOnClickListener {
-            navigate(activity, R.id.action_testFragment1_to_testFragment2)
+
+        firstBottomSheet =
+            view.findViewById<LinearLayout>(R.id.firstBottomSheet)
+        firstBehavior = BottomSheetBehavior.from(firstBottomSheet)
+
+        secondBottomSheet =
+            view.findViewById<LinearLayout>(R.id.secondBottomSheet)
+        secondBehavior = BottomSheetBehavior.from(secondBottomSheet)
+
+        firstBehavior?.state = BottomSheetBehavior.STATE_HIDDEN
+        firstBehavior?.isDraggable = true
+
+        secondBehavior?.state = BottomSheetBehavior.STATE_EXPANDED
+        secondBehavior?.isDraggable = true
+
+        secondBehavior?.addBottomSheetCallback(object : BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == STATE_EXPANDED)
+                    increaseSize()
+            }
+        })
+
+        firstBehavior?.addBottomSheetCallback(object : BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == STATE_EXPANDED)
+                    decreaseSize()
+                if (newState == STATE_HIDDEN)
+                    increaseSize()
+
+            }
+        })
+
+        view.findViewById<TextView>(R.id.txtS1).setOnClickListener {
+            firstBehavior?.state = STATE_EXPANDED
         }
-        println("$TAG onViewCreated")
+        view.findViewById<TextView>(R.id.txtS2).setOnClickListener {
+        }
+        view.findViewById<View>(R.id.rootView).setOnClickListener {
+            firstBehavior?.state = STATE_HIDDEN
+        }
     }
 
-    override fun onViewStateRestored(savedInstanceState: Bundle?) {
-        super.onViewStateRestored(savedInstanceState)
-        println("$TAG onViewStateRestored")
+    fun increaseSize() {
+        val translation = PropertyValuesHolder.ofFloat("translationY", 1f)
+        val scaleY = PropertyValuesHolder.ofFloat("scaleY", 1f)
+        val scaleX = PropertyValuesHolder.ofFloat("scaleX", 1f)
+        val view = view?.findViewById<View>(R.id.lnr1)
+        ObjectAnimator.ofPropertyValuesHolder(view, translation, scaleX, scaleY).apply {
+            duration = 300
+            start()
+        }
+
     }
 
+    fun decreaseSize() {
+        val translation = PropertyValuesHolder.ofFloat("translationY", 1f)
+        val scaleY = PropertyValuesHolder.ofFloat("scaleY", 0.9f)
+        val scaleX = PropertyValuesHolder.ofFloat("scaleX", 0.9f)
+        val view = view?.findViewById<View>(R.id.lnr1)
+        ObjectAnimator.ofPropertyValuesHolder(view, translation, scaleX, scaleY).apply {
+            duration = 300
+            start()
+        }.doOnEnd {
+            firstBehavior?.state = BottomSheetBehavior.STATE_EXPANDED
+            secondBottomSheet.isEnabled = false
+            secondBottomSheet.isClickable = false
+            secondBottomSheet.children.forEach {
+                it.isEnabled = false
+                it.isClickable = false
+            }
+        }
+    }
 }
