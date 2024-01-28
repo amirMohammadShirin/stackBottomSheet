d9cce297df7e1e9b7553a4826a0a806b89135ac9
amir mohammad shirin <45653153+amirMohammadShirin@users.noreply.github.com>
2024-01-28 22:54:24 +0330

    divide codes

--- app/src/main/java/com/example/navdemo/test/TestFragment1.kt
+++ app/src/main/java/com/example/navdemo/test/TestFragment1.kt
@@ -4,25 +4,20 @@ import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
 import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.NOT_FOCUSABLE
 import android.view.ViewGroup
-import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.animation.doOnEnd
 import androidx.core.view.children
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.example.navdemo.CustomFragmentState
 import com.example.navdemo.R
-import com.example.navdemo.navigate
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
 import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_EXPANDED
 import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_HIDDEN
-import com.google.android.material.bottomsheet.BottomSheetDialog
 
 /**
  * Created by aShirin on 1/27/2024.
@@ -31,8 +26,8 @@ class TestFragment1 : Fragment() {
 
     private val TAG = "TestFragment1"
 
-    private lateinit var firstBottomSheet: LinearLayout
-    private lateinit var secondBottomSheet: LinearLayout
+    private var firstBottomSheet: LinearLayout? = null
+    private var secondBottomSheet: LinearLayout? = null
 
     private var firstBehavior: BottomSheetBehavior<LinearLayout>? = null
     private var secondBehavior: BottomSheetBehavior<LinearLayout>? = null
@@ -48,19 +43,35 @@ class TestFragment1 : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        firstBottomSheet =
-            view.findViewById<LinearLayout>(R.id.firstBottomSheet)
-        firstBehavior = BottomSheetBehavior.from(firstBottomSheet)
+        initFirstBottomSheet()
+
+        initSecondBottomSheet()
+
+        firstBehavior.updateState(STATE_HIDDEN)
+        secondBehavior.updateState(STATE_EXPANDED)
+
+        view.findViewById<TextView>(R.id.txtS1).setOnClickListener {
+            firstBehavior.updateState(STATE_EXPANDED)
+        }
+        view.findViewById<TextView>(R.id.txtS2).setOnClickListener {
+        }
+        view.findViewById<View>(R.id.rootView).setOnClickListener {
+            firstBehavior.updateState(STATE_HIDDEN)
+        }
+    }
+
+    private fun BottomSheetBehavior<*>?.updateState(newState: Int) {
+        this?.let { it.state = newState }
+    }
+
+    private fun initSecondBottomSheet() {
 
         secondBottomSheet =
-            view.findViewById<LinearLayout>(R.id.secondBottomSheet)
-        secondBehavior = BottomSheetBehavior.from(secondBottomSheet)
+            view?.findViewById<LinearLayout>(R.id.secondBottomSheet)
 
-        firstBehavior?.state = BottomSheetBehavior.STATE_HIDDEN
-        firstBehavior?.isDraggable = true
-
-        secondBehavior?.state = BottomSheetBehavior.STATE_EXPANDED
-        secondBehavior?.isDraggable = true
+        secondBottomSheet?.let {
+            secondBehavior = BottomSheetBehavior.from(it)
+        }
 
         secondBehavior?.addBottomSheetCallback(object : BottomSheetCallback() {
             override fun onSlide(bottomSheet: View, slideOffset: Float) {
@@ -71,30 +82,6 @@ class TestFragment1 : Fragment() {
                     increaseSize()
             }
         })
-
-        firstBehavior?.addBottomSheetCallback(object : BottomSheetCallback() {
-            override fun onSlide(bottomSheet: View, slideOffset: Float) {
-
-
-            }
-
-            override fun onStateChanged(bottomSheet: View, newState: Int) {
-                if (newState == STATE_EXPANDED)
-                    decreaseSize()
-                if (newState == STATE_HIDDEN)
-                    increaseSize()
-
-            }
-        })
-
-        view.findViewById<TextView>(R.id.txtS1).setOnClickListener {
-            firstBehavior?.state = STATE_EXPANDED
-        }
-        view.findViewById<TextView>(R.id.txtS2).setOnClickListener {
-        }
-        view.findViewById<View>(R.id.rootView).setOnClickListener {
-            firstBehavior?.state = STATE_HIDDEN
-        }
     }
 
     fun increaseSize() {
@@ -117,14 +104,61 @@ class TestFragment1 : Fragment() {
         ObjectAnimator.ofPropertyValuesHolder(view, translation, scaleX, scaleY).apply {
             duration = 300
             start()
-        }.doOnEnd {
-            firstBehavior?.state = BottomSheetBehavior.STATE_EXPANDED
-            secondBottomSheet.isEnabled = false
-            secondBottomSheet.isClickable = false
-            secondBottomSheet.children.forEach {
-                it.isEnabled = false
-                it.isClickable = false
-            }
         }
     }
+
+    private fun initFirstBottomSheet() {
+        firstBottomSheet =
+            view?.findViewById<LinearLayout>(R.id.firstBottomSheet)
+        firstBottomSheet?.let {
+            firstBehavior = BottomSheetBehavior.from(it)
+        }
+        firstBehavior?.addBottomSheetCallback(object : BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == STATE_EXPANDED)
+                    decreaseSize()
+                if (newState == STATE_HIDDEN)
+                    increaseSize()
+
+            }
+        })
+    }
+
+
+}
+
+
+class SweetBottomSheet(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+
+    val behaviour = BottomSheetBehavior.from(this)
+
+
+    val callback = object : BottomSheetCallback() {
+        override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+        }
+
+        override fun onStateChanged(bottomSheet: View, newState: Int) {
+
+        }
+    }
+
+    init {
+        behaviour.addBottomSheetCallback(callback)
+    }
+
+    fun updateState(state: Int) {
+        behaviour.state = state
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        behaviour.removeBottomSheetCallback(callback)
+    }
+
 }
